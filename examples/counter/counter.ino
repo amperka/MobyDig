eacdbfd090a164ec24dd6323642762fa43d3c1b7
Yury Botov <yury.botov@gmail.com>
2020-07-17 15:04:20 +0300

    add brief and comments

--- examples/counter/counter.ino
+++ examples/counter/counter.ino
@@ -1,10 +1,20 @@
+/*
+ * This example demonstrates a simple countdown
+ */
+
+// Include the interfacing library
 #include "segm8.h"
 
+// Let's declare 1 module using hardware SPI with CS on 10 pin
 SegM8 module(10, 1);
+
 int counter = 9;
 
 void setup() {
+  // initialise SegM8
   module.begin();
+
+  // Display "StArt"
   module.display("s", 0, 1);
   delay(1000);
   module.display("t", 0, 1);
@@ -20,6 +30,8 @@ void setup() {
 void loop() {
   if (counter < 0)
     counter = 9;
+
+  // display countdown
   module.display(counter--, 0, 1);
 
   delay(1000);
